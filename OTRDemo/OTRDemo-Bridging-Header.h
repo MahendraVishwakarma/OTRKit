588b52967e2e4f07583ac64081baadad299ba3cf
amrendradevios <kumarashishpratp@gmial.com>
2019-09-03 14:54:27 +0530

    initial commit

---
+++ OTRDemo/OTRDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  OTRDemo-Bridging-Header.h
+//  OTRDemo
+//
+//  Created by Mahendra Vishwakarma on 03/09/19.
+//  Copyright © 2019 Mahendra Vishwakarma. All rights reserved.
+//
+
+#ifndef OTRDemo_Bridging_Header_h
+#define OTRDemo_Bridging_Header_h
+#import "OTRKit.h"
+#import "OTRCryptoUtility.h"
+
+#endif /* OTRDemo_Bridging_Header_h */
---
+++ OTRDemo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  OTRDemo
+//
+//  Created by Mahendra Vishwakarma on 03/09/19.
+//  Copyright © 2019 Mahendra Vishwakarma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+        let strMessage = "hi hello guys. how are you. press A for Good. press B for Sad. press C for restless"
+        let msgData = strMessage.data(using: .utf8)
+        let keyData = NSMutableData(length: 16)
+        let ivData = NSMutableData(length: 16)
+        let encryptedData  = try? OTRCryptoUtility.encryptAESGCMData(msgData!, key: keyData! as Data, iv: ivData as! Data)
+        print(encryptedData?.data)
+        
+        guard let decryptedData = try? OTRCryptoUtility.decryptAESGCMData(encryptedData!, key:  keyData! as Data, iv: ivData as! Data) else { return  }
+        print(String.init(data: decryptedData, encoding: .utf8))
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
